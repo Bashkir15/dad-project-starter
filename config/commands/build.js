8a23c6b627f6fb507cf581b70ec723b6fbdfd788
Forrest Collins <Forest.D.Collins@gmail.com>
2020-04-11 08:40:09 -0500

    Adds some slight build evaulation

--- config/commands/build.js
+++ config/commands/build.js
@@ -1,14 +1,44 @@
+const chalk = require('chalk')
+const { emptyDir } = require('fs-extra')
+const { measureFileSizesBeforeBuild, printFileSizesAfterBuild } = require('react-dev-utils/FileSizeReporter')
+
 const { 
     compileWebpack,
     createWebpackCompiler,
     createWebpackConfig
 } = require('../webpack')
 
-module.exports = async function build(context) {
-    // clear build here
-    console.log('starting build')
+const BUNDLE_GZIP_LIMIT = 512 * 1024
+const CHUNK_GZIP_LIMIT = 1024 * 1024
 
-    const compiler = createWebpackCompiler(createWebpackConfig(context))
-    const stats = await compileWebpack(compiler)
-    console.log('build finished!')
+module.exports = async function build(config) {
+    const { clientOutput } = config.paths
+
+    console.log(chalk.cyan('Evaluating the previous build output if there is one..'))
+    const previousBuildSizes = await measureFileSizesBeforeBuild((clientOutput))
+    console.log(chalk.cyan('Cleaning previous build files...'))
+    await emptyDir(config.paths.clientOutput)
+    console.log(chalk.green('Successfully remove previous build files. Begining new build'))
+
+    const compiler = createWebpackCompiler(createWebpackConfig(config))
+    const { stats, warnings } = await compileWebpack(compiler)
+
+    if (warnings.length) {
+        console.log(chalk.yellow('Compiled with warnings.\n\n'))
+        console.log(`\nSearch for the ${chalk.underline(chalk.yellow('keywords'))} to learn more about each warning`)
+        console.log(`If this is a lint warning, add ${chalk.cyan('// eslint-disable-next-line')}
+            to the line before to ignore it if you feel it necessary
+        `)
+    } else {
+        console.log(chalk.green('Build compiled successfully!'))
+    }
+
+    console.log('File sizes after compression:\n')
+    printFileSizesAfterBuild(
+        stats,
+        previousBuildSizes,
+        clientOutput,
+        BUNDLE_GZIP_LIMIT,
+        CHUNK_GZIP_LIMIT
+    )
 }
--- config/webpack/compile-webpack.js
+++ config/webpack/compile-webpack.js
@@ -1,10 +1,42 @@
+const chalk = require('chalk')
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages')
+
 module.exports = function compileWebpack(compiler) {
     return new Promise((resolve, reject) => {
         compiler.run((err, stats) => {
+            let messages
+
             if (err) {
-                return reject(err)
+                if (!err.message) return reject(err)
+
+                let errMessage = err.message
+                if (Object.prototype.hasOwnProperty.call(err, 'postcssNode')) {
+                    errMessage = `${errMessage} \n CompileError: Begins at CSS Selector
+                    ${err['postcssNode'].selector}`
+                }
+                
+                messages = formatWebpackMessages({
+                    errors: [errMessage],
+                    warnings: []
+                })
+            } else {
+                messages = formatWebpackMessages(stats.toJson({
+                    all: false,
+                    errors: true,
+                    warnings: true
+                }))
             }
-            return resolve(stats)
+
+            if (messages.errors.length) {
+                // Only keep first error. Others are often indicative of the
+                // same problem and just create noise
+                if (messages.errors.length > 1) {
+                    messages.errors.length = 1
+                }
+                return reject(new Error(messages.errors.join('\n\n')))
+            }
+
+            return { stats, warnings: messages.warnings }
         })
     })
 }
