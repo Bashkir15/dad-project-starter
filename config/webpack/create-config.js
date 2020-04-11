6b52ba854dfd30854c634763f53219287c428bd9
Forrest Collins <Forest.D.Collins@gmail.com>
2020-04-11 08:02:39 -0500

    Adds compression plugin

--- config/webpack/create-config.js
+++ config/webpack/create-config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin')
 const ExtractPlugin = require('mini-css-extract-plugin')
 const HtmlPlugin = require('html-webpack-plugin')
 
@@ -65,14 +66,40 @@ module.exports = function createConfig({ currentCommand, env, paths }) {
         plugins: [ 
             new HtmlPlugin({
                 filename: 'index.html',
+                minify: {
+                    collapseWhitespace: true,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    minifyURLs: true,
+                    removeComments: true,
+                    removeRedundantAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    useShortDoctype: true
+                },
                 template: path.join(paths.clientDir, 'index.html')
             }),
+
             currentCommand === 'dev' && new webpack.HotModuleReplacementPlugin(),
 
             env.prod && new ExtractPlugin({
                 chunkFilename: '[name].[contenthash:8].css',
                 filename: '[name].[contenthash:8].css'
+            }),
+            env.prod && new CompressionPlugin({
+                algorithm: 'gzip',
+                compressionOptions: { level: 9 },
+                filename: '[path].gz[query]',
+                minRatio: 0.8,
+                test: /\.(js|css|html|svg)$/
+            }),
+            env.prod && new CompressionPlugin({
+                algorithm: 'brotliCompress',
+                compressionOptions: { level: 11 },
+                filename: '[path].br[query]',
+                minRatio: 0.8,
+                test: /\.(js|css|html|svg)$/
             })
+
         ].filter(Boolean)
     }
 }
